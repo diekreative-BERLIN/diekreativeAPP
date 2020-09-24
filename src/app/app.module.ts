ddc3a34fe62a640bffc35d8a92f493bbc9fea236
Christian Wöhr <developer@xetan.de>
2020-09-24 22:34:43 +0200

    speichern der logindaten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { FirebaseX } from "@ionic-native/firebase-x/ngx";
@@ -19,6 +19,7 @@ import { FeedsService } from './communications/feeds.service';
 import { UserstateService } from './userstate.service';
 import { PredigtElementComponent } from './predigt-element/predigt-element.component';
 import { HTTP } from '@ionic-native/http/ngx';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 
 @NgModule({
@@ -40,7 +41,10 @@ BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
     FeedsService,
     ChurchapiService,
     UserstateService,
-    SocialSharing,InAppBrowser
+    SocialSharing,
+    InAppBrowser,
+    NativeStorage,
+    Platform
   ],
   bootstrap: [AppComponent]
 })
--- src/app/userstate.service.ts
+++ src/app/userstate.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { ChurchapiService } from './connectors/churchapi.service';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,17 @@ export class UserstateService {
   hasTuerOeffner = false;
   logintoken = "";
   fullusername = "";
-  constructor(private churchtools:ChurchapiService) {
-     
+  constructor(private churchtools:ChurchapiService, private platform :Platform, private nativeStorage: NativeStorage) {
+    platform.ready().then(() => {
+      nativeStorage.getItem('currentUser').then((user)=>{
+        this.personid = user.personid;
+        this.logintoken = user.logintoken;
+        this.fullusername = user.fullusername;
+        this.loggedin = true;
+        this.hasTuerOeffner = user.hasTuerOeffner;
+      })
+
+    });
   }
 
   public userLogginSuccessful(personid){
@@ -22,7 +33,11 @@ export class UserstateService {
       this.churchtools.getPersonData(personid).then((res)=>{
         console.log("persondata:" +JSON.stringify(JSON.parse(res.data)));
         this.fullusername = (JSON.parse(res.data)).data.firstName + " " +(JSON.parse(res.data)).data.lastName
-        
+        this.nativeStorage.setItem('currentUser', {personid: this.personid, logintoken: this.logintoken, fullusername: this.fullusername, hasTuerOeffner:this.hasTuerOeffner})
+  .then(
+    () => console.log('Stored item!'),
+    error => console.error('Error storing item', error)
+  );
       })
       var resultObj = JSON.parse(res.data)
       console.log("Get Group for Loggedin " +JSON.stringify(resultObj));
@@ -34,6 +49,11 @@ export class UserstateService {
         if( "9" == group?.group.domainIdentifier ) {
           console.log("has Zugang");
           hasZugang = true;
+          this.nativeStorage.setItem('currentUser', {personid: this.personid, logintoken: this.logintoken, fullusername: this.fullusername, hasTuerOeffner:this.hasTuerOeffner})
+  .then(
+    () => console.log('Stored item!'),
+    error => console.error('Error storing item', error)
+  );
         }
       })
       if(hasZugang){
@@ -42,6 +62,11 @@ export class UserstateService {
           console.log(JSON.stringify(res.data));
           this.logintoken = JSON.parse(res.data).data
           console.log("Has Tueroeffner and Logintoken");
+          this.nativeStorage.setItem('currentUser', {personid: this.personid, logintoken: this.logintoken, fullusername: this.fullusername, hasTuerOeffner:this.hasTuerOeffner})
+  .then(
+    () => console.log('Stored item!'),
+    error => console.error('Error storing item', error)
+  );
         }).catch((err)=>{
           console.log("error on retrieving logintoken");
           this.loggedin = false;
@@ -58,6 +83,11 @@ export class UserstateService {
       this.logintoken = "";
       console.log("dropped here " + JSON.stringify(err));
     });
+    this.nativeStorage.setItem('currentUser', {personid: this.personid, logintoken: this.logintoken, fullusername: this.fullusername, hasTuerOeffner:this.hasTuerOeffner})
+  .then(
+    () => console.log('Stored item!'),
+    error => console.error('Error storing item', error)
+  );
   }
 
   public userLoggedOut(){
@@ -65,6 +95,7 @@ export class UserstateService {
     this.personid = 0;
     this.hasTuerOeffner = false;
     this.logintoken = "";
+    this.nativeStorage.remove('currentUser');
   }
 }
 
