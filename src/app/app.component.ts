65e7a2dfcf6ab066d288263a262e791083b7fbc1
Christian Wöhr <developer@xetan.de>
2020-11-09 03:59:38 +0100

    solved the Notification on iOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,7 +44,9 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.firebaseX.grantPermission();
       this.firebaseX.getToken()
+    
   .then(token => {
                  console.log(`The token is ${token}`);}) // save the token server-side and use it to push notifications to this device
   .catch(error => alert(`Error getting token ${error}`));
