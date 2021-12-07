609fe813ae36abbe1101ed8b4bd1f5e7ca9caf4b
Uche Okereke <orezpablo@gmail.com>
2021-12-07 10:27:19 +0100

    style: formatting predigten-audio for readability

--- src/app/predigten-audio/predigten-audio.page.ts
+++ src/app/predigten-audio/predigten-audio.page.ts
@@ -1,32 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FeedsService } from '../communications/feeds.service';
-import { NewsRss } from '../communications/news-rss';
-
+import { Component, OnInit } from "@angular/core";
+import { FeedsService } from "../communications/feeds.service";
+import { NewsRss } from "../communications/news-rss";
 
 @Component({
-  selector: 'app-predigten-audio',
-  templateUrl: './predigten-audio.page.html',
-  styleUrls: ['./predigten-audio.page.scss'],
+  selector: "app-predigten-audio",
+  templateUrl: "./predigten-audio.page.html",
+  styleUrls: ["./predigten-audio.page.scss"],
 })
 export class PredigtenAudioPage implements OnInit {
-  rssData:NewsRss;
+  rssData: NewsRss;
   predigten;
-  constructor(private feedservice:FeedsService) { 
-    
-  }
+  constructor(private feedservice: FeedsService) {}
 
   ngOnInit() {
     this.getRssData();
   }
 
-  
   getRssData() {
     this.rssData = this.feedservice.RssData;
-    this.predigten = this.rssData.rss.channel[0].item
+    this.predigten = this.rssData.rss.channel[0].item;
     console.log(JSON.stringify(this.rssData));
   }
-
-  
-
-
 }
